5507e3d0677060b37789dbdb731f7e9d6e21d01c
Horacio Borrego <hborrego365@gmail.com>
2023-01-23 16:45:10 -0800

    Install QRcode via CDN in html file, Create generateQRcode function and clearUI function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict'
+// 'use strict'
+
+const qr = document.getElementById('qrcode')
+const form = document.getElementById('qr-form')
+form.addEventListener('submit', generateQRcode)
+
+function generateQRcode(e) {
+  e.preventDefault()
+  clearUI()
+  const url = document.getElementById('url-input').value
+
+  if (url === '') return alert('Please enter a url')
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    colorDark: '#fff',
+    colorLight: 'rgb(74, 133, 276)',
+    width: '200',
+    height: '200',
+  })
+}
+
+function clearUI() {
+  qr.innerHTML = ''
+}
